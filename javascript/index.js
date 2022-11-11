87abd3dc4b2c7cf7309e0d74b42a254e2bc47a2e
Bouteille Thomas <btltoma@gmail.com>
2022-11-11 14:02:59 +0100

    add display selection

--- javascript/index.js
+++ javascript/index.js
@@ -4,44 +4,61 @@ const WIDTH = 700
 const HEIGHT = 500
 let centered;
 
+let dataBati;
+let dataQuart;
+
+let displayQuart = true;
+let quartGroupe;
+
 window.onload = function () {
 
 
     [...document.getElementsByClassName('menu')].forEach(elem => {
+
+
         elem.onclick = (e) => {
             let selectedElem = document.getElementsByClassName('selected')[0];
             let isFirst = selectedElem === document.getElementsByClassName('menu')[0]
-            console.log(isFirst)
-            if(isFirst && selectedElem !== e.target){
+            if (isFirst && selectedElem !== e.target) {
                 let menuBar = document.getElementsByClassName('menu-bottom-border')[0];
                 let leftOffset = parseInt(menuBar.style.left.match(/\d*/));
                 menuBar.style.left = `${leftOffset + 200}px`
             }
-            else{
-                if(!isFirst && selectedElem !== e.target){
+            else {
+                if (!isFirst && selectedElem !== e.target) {
                     let menuBar = document.getElementsByClassName('menu-bottom-border')[0];
                     let leftOffset = parseInt(menuBar.style.left.match(/\d*/));
-                    console.log(leftOffset)
                     menuBar.style.left = `${leftOffset - 200}px`
                 }
             }
+
+            displayQuart = elem.dataset.type === 'quartier';
+            if (!displayQuart) {
+                displayMainBati(dataBati, quartGroupe);
+            }
+            else {
+                removeBati()
+            }
+
             selectedElem.classList.remove('selected');
             e.target.classList.add('selected')
         }
     });
 
-
-
-
-
+    function removeBati() {
+        d3.select('#bati').remove()
+    }
 
     getDataFromFile('q').then(quartiersData => {
+        dataQuart = quartiersData;
         getDataFromFile('b').then(batiData => {
-            console.log(batiData)
+            dataBati = batiData;
             const svg = d3.select('#map').append('svg').attr('width', WIDTH).attr('height', HEIGHT);
-            const g = svg.append('g');  
-            let group = displayMainQuart(quartiersData, g)
-            displayMainBati(batiData, group);
+            const g = svg.append('g');
+            quartGroupe = displayMainQuart(quartiersData, g)
+            if (!displayQuart) {
+                displayMainBati(batiData, quartGroupe);
+            }
 
             /* var zoom = d3.zoom()
                 .scaleExtent([1,8])
@@ -53,14 +70,14 @@ window.onload = function () {
             svg.call(zoom); */
 
         }).catch(err => {
-            console.error(err);s
+            console.error(err); s
         });
-        
+
     }).catch(err => {
         console.error(err);
     });
 
-    function displayMainBati(featureCollection, container){
+    function displayMainBati(featureCollection, container) {
         let projection = d3.geoMercator()
             .fitSize([WIDTH, HEIGHT], featureCollection)
         let path = d3.geoPath().projection(projection)
@@ -78,93 +95,92 @@ window.onload = function () {
             .data(featureCollection.features)
             .enter()
             .append('path')
-                .classed('active', true)
-                .attr('d', path)
-                .attr('fill', d => COLORS[d.properties.level])
-                .attr('data-quart', d => d.properties.quart_name)
-                
+            .classed('active', true)
+            .attr('d', path)
+            .attr('fill', d => COLORS[d.properties.level])
+            .attr('data-quart', d => d.properties.quart_name)
     }
 
-    function displayMainQuart(featureCollection, container){
+    function displayMainQuart(featureCollection, container) {
         let projection = d3.geoMercator()
             .fitSize([WIDTH, HEIGHT], featureCollection)
         let path = d3.geoPath().projection(projection)
+
+        console.log(featureCollection.features);
 
         const g = container.append('g').attr('id', 'quart');
         g.selectAll('path')
             .data(featureCollection.features)
             .enter()
             .append('path')
-            .attr('d', path)
-            .attr('fill', 'transparent')
-            .attr('stroke', getComputedStyle(document.documentElement).getPropertyValue('--text'))
-            .on('click', function(_, d){
-                clickedQuart(d, g, path)
-            })
+                .attr('d', path)
+                .attr('fill', 'transparent')
+                .attr('stroke', getComputedStyle(document.documentElement).getPropertyValue('--text'))
+                .attr('data-quart', d => d.properties.quart_name)
+                .on('click', function (_, d) {
+                    clickedQuart(d, g, path)
+                })
+                .on('mouseover', function (d, i) {
+                    d3.select(this).transition()
+                        .duration('50')
+                        .attr('fill', d.properties)
+                })
+                .on('mouseout', function (d, i) {
+                    d3.select(this).transition()
+                        .duration('50')
+                        .attr('fill', 'transparent')
+                });
         return g;
     }
 
-    function clickedQuart(d, container, path){
+    function clickedQuart(d, container, path) {
         let x, y, k;
 
-        if(d && centered !== d){
+        if (d && centered !== d) {
             let centroid = path.centroid(d);
             x = centroid[0];
             y = centroid[1];
             k = 4
             centered = d;
         }
-        else{
+        else {
             x = WIDTH / 2;
             y = HEIGHT / 2;
             k = 1;
             centered = null;
         }
 
-        container.select('#quart').selectAll('path')
-            .classed('active', centered && function(d){return d === centered})
-        
+        let quartName = 'Namur'
         if(centered){
+            quartName = d.properties.quart_name
+        }
+        document.getElementById('selection-name').innerHTML = quartName;
+
+        container.select('#quart').selectAll('path')
+            .classed('active', centered && function (d) { return d === centered })
+
+        if (centered) {
             container.selectAll(`#bati :not(path[data-quart="${d.properties.quart_name}"])`)
                 .style('display', 'none')
+            container.selectAll(`#quart :not(path[data-quart="${d.properties.quart_name}"])`)
+                .style('display', 'none')
             container.selectAll(`#bati path[data-quart="${d.properties.quart_name}"]`)
+                .style('display', 'block')
+            container.selectAll(`#quart path[data-quart="${d.properties.quart_name}"]`)
                 .style('display', 'block')
         }
 
         container.transition()
             .duration(1000)
             .attr("transform", "translate(" + WIDTH / 2 + "," + HEIGHT / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-            .on('end', function(){
-                if(!centered){
-                    container.selectAll('#bati path').style('display','block')
+            .on('end', function () {
+                if (!centered) {
+                    container.selectAll('#bati path').style('display', 'block')
+                    container.selectAll('#quart path').style('display', 'block')
                 }
             })
 
     }
-
-    function selectLocality(data, container){
-
-        let featureCollection = {type:'FeatureCollection', 'features':[data]}
-
-        let projection = d3.geoMercator()
-            .fitSize([WIDTH, HEIGHT], featureCollection)
-        let path = d3.geoPath().projection(projection)
-
-        container.selectAll('*').remove();
-        const g = container.append('g').append('g');
-
-        g.selectAll('path')
-            .data(featureCollection.features)
-            .enter()
-            .append('path')
-            .attr('d', path)
-            .attr('fill', 'transparent')
-            .attr('stroke', getComputedStyle(document.documentElement).getPropertyValue('--text'))
-
-        console.log(data)           
-        document.getElementById('selection-name').innerHTML = data.properties.quart_name;
-    }
-    
 
 
     /*
@@ -177,8 +193,8 @@ window.onload = function () {
     })
     .attr('stroke-width', 4)
     */
-    
-   
+
+
     /*
     var width = 960
     var height = 500
